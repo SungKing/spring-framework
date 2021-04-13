6fe3162a098f0dc4d4cc6e621451feff91224372
王松 <song.wang@mtime.com>
2021-04-13 16:46:36 +0800

    SpringFactoriesLoader 解读

--- spring-core/src/main/java/org/springframework/core/io/support/SpringFactoriesLoader.java
+++ spring-core/src/main/java/org/springframework/core/io/support/SpringFactoriesLoader.java
@@ -41,6 +41,7 @@ import org.springframework.util.StringUtils;
 
 /**
  * General purpose factory loading mechanism for internal use within the framework.
+ * 在框架内内部使用的通用工厂装载器
  *
  * <p>{@code SpringFactoriesLoader} {@linkplain #loadFactories loads} and instantiates
  * factories of a given type from {@value #FACTORIES_RESOURCE_LOCATION} files which
@@ -53,6 +54,9 @@ import org.springframework.util.StringUtils;
  *
  * where {@code example.MyService} is the name of the interface, and {@code MyServiceImpl1}
  * and {@code MyServiceImpl2} are two implementations.
+ *
+ * SpringFactoriesLoader   加载并且实例化 类路径中jar包俩面的 FACTORIES_RESOURCE_LOCATION 文件 的 工厂
+ * spring.factories 必须是 Properties 格式，key必须是接口或抽象类的全名，value 必须是一系列实现累
  *
  * @author Arjen Poutsma
  * @author Juergen Hoeller
@@ -80,12 +84,17 @@ public final class SpringFactoriesLoader {
 	/**
 	 * Load and instantiate the factory implementations of the given type from
 	 * {@value #FACTORIES_RESOURCE_LOCATION}, using the given class loader.
+	 * 用给定的类加载器 加载和实例化 FACTORIES_RESOURCE_LOCATION 里面的给定类型的工厂实现
+	 *
 	 * <p>The returned factories are sorted through {@link AnnotationAwareOrderComparator}.
+	 * 返回值根据AnnotationAwareOrderComparator 排序
 	 * <p>If a custom instantiation strategy is required, use {@link #loadFactoryNames}
 	 * to obtain all registered factory names.
+	 * 如果需要自定义实现策略，用loadFactoryNames 来查看所有的工厂名称
 	 * <p>As of Spring Framework 5.3, if duplicate implementation class names are
 	 * discovered for a given factory type, only one instance of the duplicated
 	 * implementation type will be instantiated.
+	 * 从 5.3 开始, 如果给定类型 有2重复的实现类的名称， 他们中仅有一个里面的实例会被实例化
 	 * @param factoryType the interface or abstract class representing the factory
 	 * @param classLoader the ClassLoader to use for loading (can be {@code null} to use the default)
 	 * @throws IllegalArgumentException if any factory implementation class cannot
@@ -114,9 +123,11 @@ public final class SpringFactoriesLoader {
 	 * Load the fully qualified class names of factory implementations of the
 	 * given type from {@value #FACTORIES_RESOURCE_LOCATION}, using the given
 	 * class loader.
+	 * 用给定的类加载器，加载给定类型的实现类的全名称
 	 * <p>As of Spring Framework 5.3, if a particular implementation class name
 	 * is discovered more than once for the given factory type, duplicates will
 	 * be ignored.
+	 * 5.3开始， 如果给定工厂类型的 实现类被发现多次，则重复的将会被忽略
 	 * @param factoryType the interface or abstract class representing the factory
 	 * @param classLoader the ClassLoader to use for loading resources; can be
 	 * {@code null} to use the default
@@ -140,15 +151,15 @@ public final class SpringFactoriesLoader {
 
 		result = new HashMap<>();
 		try {
-			Enumeration<URL> urls = classLoader.getResources(FACTORIES_RESOURCE_LOCATION);
+			Enumeration<URL> urls = classLoader.getResources(FACTORIES_RESOURCE_LOCATION);//发现所有的 spring.factories 资源
 			while (urls.hasMoreElements()) {
 				URL url = urls.nextElement();
 				UrlResource resource = new UrlResource(url);
-				Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+				Properties properties = PropertiesLoaderUtils.loadProperties(resource);//读取文件
 				for (Map.Entry<?, ?> entry : properties.entrySet()) {
 					String factoryTypeName = ((String) entry.getKey()).trim();
 					String[] factoryImplementationNames =
-							StringUtils.commaDelimitedListToStringArray((String) entry.getValue());
+							StringUtils.commaDelimitedListToStringArray((String) entry.getValue());//逗号分割
 					for (String factoryImplementationName : factoryImplementationNames) {
 						result.computeIfAbsent(factoryTypeName, key -> new ArrayList<>())
 								.add(factoryImplementationName.trim());
@@ -156,7 +167,7 @@ public final class SpringFactoriesLoader {
 				}
 			}
 
-			// Replace all lists with unmodifiable lists containing unique elements
+			// Replace all lists with unmodifiable lists containing unique elements //用去重复的不可变的集合代替
 			result.replaceAll((factoryType, implementations) -> implementations.stream().distinct()
 					.collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList)));
 			cache.put(classLoader, result);
@@ -167,16 +178,16 @@ public final class SpringFactoriesLoader {
 		}
 		return result;
 	}
-
+	//对工厂实例化
 	@SuppressWarnings("unchecked")
 	private static <T> T instantiateFactory(String factoryImplementationName, Class<T> factoryType, ClassLoader classLoader) {
 		try {
-			Class<?> factoryImplementationClass = ClassUtils.forName(factoryImplementationName, classLoader);
-			if (!factoryType.isAssignableFrom(factoryImplementationClass)) {
+			Class<?> factoryImplementationClass = ClassUtils.forName(factoryImplementationName, classLoader); //根据名字查找类
+			if (!factoryType.isAssignableFrom(factoryImplementationClass)) {//实现类是否真的实现 此type
 				throw new IllegalArgumentException(
 						"Class [" + factoryImplementationName + "] is not assignable to factory type [" + factoryType.getName() + "]");
 			}
-			return (T) ReflectionUtils.accessibleConstructor(factoryImplementationClass).newInstance();
+			return (T) ReflectionUtils.accessibleConstructor(factoryImplementationClass).newInstance();//普通的通过反射来实例化
 		}
 		catch (Throwable ex) {
 			throw new IllegalArgumentException(
